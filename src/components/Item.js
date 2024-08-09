148fd72dc3ba6579672010f5e179f8dea327ba72
Sundeep <sundeep.kaler@gmail.com>
2024-08-09 18:48:36 -0400

    complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,24 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Item({ name, category }) {
-  const [isInCart, setIsInCart] = useState(false);
-
-  function handleAddToCartClick() {
-    setIsInCart((isInCart) => !isInCart);
+//function Item({ name, category }) {
+class Item extends React.Component {
+  
+  //const [isInCart, setIsInCart] = useState(false);
+  state = {
+    isInCart: false
   }
 
-  return (
-    <li className={isInCart ? "in-cart" : ""}>
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button
-        className={isInCart ? "remove" : "add"}
-        onClick={handleAddToCartClick}
-      >
-        {isInCart ? "Remove From" : "Add to"} Cart
-      </button>
-    </li>
-  );
+  handleAddToCartClick() {
+    this.setState({isInCart: !this.state.isInCart })
+    //setIsInCart((isInCart) => !isInCart);
+  }
+  render () {
+    return (
+      <li className={this.state.isInCart ? "in-cart" : ""}>
+        <span>{this.props.name}</span>
+        <span className="category">{this.props.category}</span>
+        <button
+          className={this.state.isInCart ? "remove" : "add"}
+          onClick={() => this.handleAddToCartClick()}
+        >
+          {this.state.isInCart ? "Remove From" : "Add to"} Cart
+        </button>
+      </li>
+    );
+  }
+  
 }
 
 export default Item;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,38 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-
-  function handleCategoryChange(event) {
-    // event.target.value will be the value selected by the user
-    setSelectedCategory(event.target.value);
+//function ShoppingList({ items }) {
+  //const [selectedCategory, setSelectedCategory] = useState("All");
+class ShoppingList extends React.Component {
+  state = {
+    category: "All"
   }
 
-  // we want to filter the items to only display the ones based on the selected category
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  handleCategoryChange(event) {
+    // event.target.value will be the value 
+    //selected by the user
+    this.setState({category: event.target.value})
+    //setSelectedCategory(event.target.value);
+  }
 
-    return item.category === selectedCategory;
-  });
+  render () {  
+    // we want to filter the items to only display the ones based on the selected category
+    const itemsToDisplay = this.props.items.filter((item) => {
+      if (this.state.category === "All") return true;
 
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+      return item.category === this.state.category;
+    });
+
+    return (
+      <div className="ShoppingList">
+        <div className="Filter">
+          <select name="filter" onChange={(e) => this.handleCategoryChange(e)}>
+            <option value="All">Filter by category</option>
+            <option value="Produce">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Dessert">Dessert</option>
+          </select>
+        </div>
+        <ul className="Items">
+          {itemsToDisplay.map((item) => (
+            <Item key={item.id} name={item.name} category={item.category} />
+          ))}
+        </ul>
       </div>
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
 export default ShoppingList;
